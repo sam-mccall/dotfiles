6754324bfd43d10da3b87455cf6443b36ce04c76
Sam McCall <sam.mccall@gmail.com>
2021-12-23 23:12:43 +0100

    nvim

---
+++ .config/nvim/init.vim
@@ -0,0 +1,51 @@
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+lua require('plugins')
+
+color monokai
+hi Comment ctermfg=179
+hi NormalFloat ctermbg=236
+hi LspReferenceText ctermbg=24
+hi link LspReferenceRead LspReferenceText
+hi link LspReferenceWrite LspReferenceText
+
+set mouse=a
+set nowrap
+set colorcolumn=80,100 cursorline
+set number
+set expandtab tabstop=2 shiftwidth=2 softtabstop=2 smarttab
+set list listchars=tab:▸\ ,trail:·,extends:>,precedes:<
+set ignorecase smartcase
+set incsearch hlsearch
+nnoremap <C-_> :nohlsearch<CR>
+nnoremap <C-C> :bd<CR>
+
+let wildmode="longest:full,full"
+
+nnoremap ,sh <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap ,sd <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap ,sr <cmd>lua vim.lsp.buf.references()<CR>
+nnoremap ,sn <cmd>lua vim.lsp.buf.rename()<CR>
+nnoremap ,sa <cmd>lua vim.lsp.buf.code_action()<CR>
+
+nnoremap ,b :ls<CR>:b 
+nnoremap \ :NvimTreeToggle<CR>
+
+nnoremap ,ff <cmd>Telescope git_files theme=dropdown<cr>
+nnoremap ,fg <cmd>Telescope live_grep theme=dropdown<cr>
+nnoremap ,fb <cmd>Telescope buffers theme=dropdown<cr>
+nnoremap ,fh <cmd>Telescope help_tags theme=dropdown<cr>
+nnoremap ,fs <cmd>Telescope lsp_dynamic_workspace_symbols theme=dropdown<cr>
+nnoremap ,fr <cmd>Telescope lsp_references theme=dropdown<cr>
+
+set updatetime=200
+autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+
+imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)':'<Tab>'
+smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)':'<Tab>'
+imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)':'<Tab>'
+smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)':'<Tab>'
+
+nnoremap <C-PageUp> :TablineBufferPrevious<CR>
+nnoremap <C-PageDown> :TablineBufferNext<CR>
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,125 @@
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use {'neovim/nvim-lspconfig',
+    config = function()
+      require('lspconfig').clangd.setup {
+        handlers = {
+          ['textDocument/clangd.fileStatus'] = function(err, result, ctx, cfg)
+            local buf = vim.fn.bufnr(vim.uri_to_fname(result.uri))
+            if buf < 0 then return end
+            local state = ''
+            if result.state == 'idle' then
+              state = '✓'
+            elseif string.find(result.state, 'parsing include', 0, true) then
+              state = '⟳'
+            elseif string.find(result.state, 'queued', 0, true) then
+              state = '…'
+            end
+            vim.b[buf].lsp_status = state
+          end
+        },
+        init_options = { clangdFileStatus = true },
+      }
+    end,
+  }
+
+  use {'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/vim-vsnip'
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+        }
+      })
+    end,
+  }
+
+  use {'ray-x/lsp_signature.nvim',
+    config = function()
+      require('lsp_signature').setup()
+    end,
+  }
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    setup = function()
+      vim.g.nvim_tree_show_icons = {
+        folder_arrows = 0,
+        folders = 1,
+      }
+      vim.g.nvim_tree_respect_buf_cwd = 1
+    end,
+    config = function()
+      require('nvim-tree').setup {
+        view = { width = 30 },
+        update_cwd = true,
+        update_focused_file = { enable = true, },
+        update_to_buf_dir = { enable = true, auto_open = true },
+      }
+    end,
+  }
+
+  use {'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          path_display = {"smart"},
+        },
+      }
+    end,
+  }
+
+  use {'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'onedark',
+          disabled_filetypes = {'NvimTree','packer'},
+        },
+        sections = {
+          lualine_x = {'filetype'},
+          lualine_y = {function()
+            for _, client in ipairs(vim.lsp.buf_get_clients()) do
+              local result = client.name
+              local state = vim.b.lsp_status
+              if state then
+                result = result .. ' ' .. state
+              end
+              return result
+            end
+            return 'x'
+          end},
+          lualine_z = {'progress', 'location'},
+        },
+      }
+    end
+  }
+
+  use {'kdheepak/tabline.nvim',
+    config = function()
+      require('tabline').setup {
+        options = {
+          show_tabs_always = false,
+          show_filename_only = true,
+        }
+      }
+    end,
+  }
+
+end)
+
